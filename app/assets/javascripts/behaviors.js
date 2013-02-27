81d5a1a6a3fab487394223625124b11b9077a550
paul@brdp.com <paul@brdp.com>
2013-02-27 01:02:42 -0600

    Working beta
    jQuery now hiding and showing exact matches.

---
+++ app/assets/javascripts/behaviors.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+		$('#searchTerm').on("keyup", function() {
+			var searchTerm = $(this).val();
+			
+			//As user types, show anything matching in the div class=tonerinfo that user types
+			//into the search input box, hide none matching.
+			
+			$('.tonerinfo:contains(' + searchTerm + ')').show();
+			$('.tonerinfo:not(:contains(' + searchTerm + '))').hide();
+			
+			
+		});
+	});
